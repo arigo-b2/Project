96c3c8f7056324473ad7a83efb84b3258beac9a2
arigo-b2 <arbinagotame2056@gmail.com>
2025-04-17 20:53:39 +1000

    Added method calculate_average_diameter
    Added method calculate_average_diameter

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -6,3 +6,13 @@ def calculate_average_diameter(row):
     Calculate average estimated diameter in km.
     """
     return (row['diameter_min_km'] + row['diameter_max_km']) / 2
+
+def calculate_kinetic_energy(row):
+    """
+    Estimate kinetic energy using: KE ~ 0.5 * mass * velocity^2
+    Assume mass ∝ diameter^3 (simplified proxy).
+    """
+    diameter = row['avg_diameter_km']
+    velocity = row['velocity_km_s']
+    estimated_mass = diameter ** 3
+    return 0.5 * estimated_mass * (velocity ** 2)
