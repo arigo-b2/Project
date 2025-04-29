25c511b4c19f9818b303df0ced9de17f47a8fae4
arigo-b2 <arbinagotame2056@gmail.com>
2025-04-29 19:27:03 +1000

    update Interactive 3d description
    update Interactive 3d description

--- app/app.py
+++ app/app.py
@@ -126,7 +126,7 @@ with tab1:
     st.markdown("---")
     
     # NASA visual
-    st.markdown("### 🌌 NASA Asteroid Interactive 3D Viewer: Next Five Closest Asteroid Approaches to Earth")
+    st.markdown("### 🌌 NASA Interactive 3D Viewer: Visualising the Next Five Closest Asteroid Flybys")
     components.iframe("https://eyes.nasa.gov/apps/asteroids/#/watch/", height=650)
 
 
